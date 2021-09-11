66c090956473f80ced8c9de935abeba9449b8bec
AledPeart <aledpeart@gmail.com>
2021-09-11 14:25:03 +0000

    Added updates folowing along to the Torin & Co video lessons: advancved routing, processing forms and user feedback

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template  #imports the Flask class
+import json
+from flask import Flask, render_template, request  #imports the Flask class
 
 app = Flask (__name__) #creates an instance of the class Flask (capital letter denotes a class) and stores it in a vraiabe (app)
                         # uses a default python variable __name__ Flask needs this to find templates and static files
@@ -11,11 +12,27 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3], my_string ="is the magic number")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data) 
+        for obj in data: 
+            if obj["url"] == member_name:
+                member = obj
+    return  render_template("member.html", member=member) 
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
